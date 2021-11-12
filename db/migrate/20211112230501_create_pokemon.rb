d305f0216a0d6e4dc748f7f80bc1c459320aa1d0
will14lee <will14lee@gmail.com>
2021-11-12 18:12:42 -0500

    Created pokemons table along with table columns

--- db/migrate/20211112230501_create_pokemon.rb
+++
@@ -1,4 +0,0 @@
-class CreatePokemon < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
---
+++ db/migrate/20211112230501_create_pokemons.rb
@@ -0,0 +1,10 @@
+class CreatePokemons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.string :type
+      t.integer :level
+      t.integer :pokemon_trainer_id
+      t.timestamps
+  end
+end
