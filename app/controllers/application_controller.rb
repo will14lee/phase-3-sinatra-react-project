45cc0476d9be30c4f990835ae3ae7fc90be5f68d
will14lee <will14lee@gmail.com>
2021-11-12 21:01:09 -0500

    Added Pokemon_trainers route which display all the current Pokemon trainers including all of their pokemons' names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,35 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    "Hello welcome to the Pokemon API!"
   end
+
+  get "/pokemons" do
+    pokemons= Pokemon.all
+    pokemons.to_json(include: {
+      pokemon_trainer: { only: [:name]}
+      })
+  end
+
+  get "/pokemons/:id" do
+    pokemon= Pokemon.find_by(id: params[:id])
+    pokemon.to_json(include: {
+      pokemon_trainer: { only: [:name]}
+      })
+  end
+
+get "/strongest_pokemon" do
+  strongest_pokemon= Pokemon.order(level: :desc).limit(1)
+  strongest_pokemon[0].to_json(include: {
+    pokemon_trainer: { only: [:name]}
+    })
+end
+
+get "/pokemon_trainers" do
+  pokemon_trainers= PokemonTrainer.all
+  pokemon_trainers.to_json(include: {
+    pokemons: { only: [:name]}
+    })
+end
 
 end
